5ef470a394849fefabbc71dbaca0a9ed0a512e2d
Angelee Shaw <angeleefshaw@gmail.com>
2021-04-01 16:55:09 -0400

    Removed Computer Science

--- client/src/Components/Topic/Topic.js
+++ client/src/Components/Topic/Topic.js
@@ -31,15 +31,6 @@ return (
       <Button variant="primary">Take the Quiz</Button>
     </Card.Body>
   </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Text>
-        Computer Science
-      </Card.Text>
-      <Button variant="primary">Take the Quiz</Button>
-    </Card.Body>
-  </Card>
 </CardDeck>
 )
 }
