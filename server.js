3e0411108f0ab5e2f7118149dce491e8e9f674f8
abissier <71886314+abissier@users.noreply.github.com>
2021-04-01 18:20:51 -0700

    corrected path to passport

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const path = require('path');
 var session = require('express-session');
 var passport = require('./config-2/passport');
 
-
-
-const express = require("express");
-const routes = require("./routes");
-const path = require("path");
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -26,16 +20,13 @@ app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // Define API routes here
 
 //routes
 // require("./routes/routes.js")(app);
 
-
 // Add routes, both API and view
-app.use(routes);
-
+// app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
