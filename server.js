f152839c553ed70d792e103c0ce6e9d8023ab0dd
abissier <71886314+abissier@users.noreply.github.com>
2021-04-01 21:14:02 -0700

    connected back and front end

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
 var session = require('express-session');
+
 var passport = require('./config-2/passport');
 const path = require('path');
-const routes = require("./routes");
-
-
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Requiring our models for syncing
-var db = require('./models');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -18,22 +14,29 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
+
+// Add routes, both API and view
+app.use(routes);
+
+// Requiring our models for syncing
+var db = require('./models');
+
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-//Add routes, both API and view
-app.use(routes);
-
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
 
-db.sequelize.sync({ force: false }).then(function() {
-	app.listen(PORT, function() {
-		console.log('App listening on PORT ' + PORT);
-	});
+// db.sequelize.sync({ force: false }).then(function() {
+// 	app.listen(PORT, function() {
+// 		console.log('App listening on PORT ' + PORT);
+// 	});
+// });
+
+app.listen(PORT, function() {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
