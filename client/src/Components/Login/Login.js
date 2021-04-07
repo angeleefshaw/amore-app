61f1431d0cc44ca271bc2e06a1a29f3f2bed1355
abissier <71886314+abissier@users.noreply.github.com>
2021-04-06 21:34:11 -0700

    signup passport route

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import './login.css';
 
 function Login() {
+	const [ loginUsername, setLoginUsername ] = useState('');
+	const [ loginPassword, setLoginPassword ] = useState('');
+
+	const login = () => {
+		axios
+			.post('/api/login', {
+				username: loginUsername,
+				password: loginPassword
+			})
+			.then(
+				function() {
+					window.location.href = '/Main';
+				}.catch(function(err) {
+					console.log(err);
+				})
+			);
+	};
+
 	return (
-		<div className = "container">
 		<div className="login-card">
-			<h3>User Sign In</h3>
+			<h1>User Sign In</h1>
 			<form className="pure-form pure-form-aligned">
 				<fieldset>
 					<div className="pure-control-group">
-						<label htmlFor="aligned-name">Username: </label>
-						<input className= "inputName" type="text" id="aligned-name" placeholder="Username" />
+						<label htmlFor="aligned-name">Username</label>
+						<input
+							type="text"
+							id="aligned-name"
+							placeholder="Username"
+							onChange={(e) => setLoginUsername(e.target.value)}
+						/>
 					</div>
 					<div className="pure-control-group">
-						<label htmlFor="aligned-password">Password: </label>
-						<input  className = "inputPass" type="password" id="aligned-password" placeholder="Password" />
+						<label htmlFor="aligned-password">Password</label>
+						<input
+							type="password"
+							id="aligned-password"
+							placeholder="Password"
+							onChange={(e) => setLoginPassword(e.target.value)}
+						/>
 					</div>
-					<button variant="outline-dark" href="/main">
+					<Button variant="outline-dark" href="/main" onClick={login}>
 						Sign In
-					</button>
+					</Button>
 				</fieldset>
 			</form>
-		</div>
 		</div>
 	);
 }
