edb14e3b8ae12902199f5ce14493a6ffea57e5a9
Andree Dantzler <andree.balayssac@icloud.com>
2021-04-24 19:22:33 -0400

    updating questions so it renders one at the time

--- client/src/Components/Questions/Questions.js
+++ client/src/Components/Questions/Questions.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Component } from "react";
-import {ListGroup, Button, PageItem} from "react-bootstrap";
-import { withRouter } from "react-router-dom"; 
+import { ListGroup, Button, PageItem } from "react-bootstrap";
+import { withRouter } from "react-router-dom";
 import API from "../../utils/API";
 
 class Questions extends Component {
@@ -10,15 +10,14 @@ class Questions extends Component {
     super(props);
     this.state = {
       questions: [],
-      currentQuestionsIndex : 0,
+      currentQuestionIndex: 0,
       score: 0,
     };
   }
 
   fetchQuestions = () => {
-
     API.getQuestion(this.props.topic).then((res) => {
-      console.log(res) 
+      console.log(res);
       this.setState({
         questions: res.data,
       });
@@ -27,38 +26,37 @@ class Questions extends Component {
 
   componentDidMount() {
     this.fetchQuestions();
-    console.log(this.state.questions)
+    console.log(this.state.questions);
   }
 
   handleSelection = (event) => {
     const { target } = event;
-    if (!target.parentElement.disabled) {
-      const correct = target.getAttribute("data-correct") === "true";
-      if (correct) {
-        target.classList.add("list-group-item-success");
-        this.incrementScore();
-      } else {
-        target.classList.add("list-group-item-danger");
-      }
-      target.parentElement.disabled = true;
+    const correct = target.getAttribute("data-correct") === "true";
+    if (correct) {
+      this.incrementScore();
     }
+    this.incrementQuestionIndex();
   };
 
   incrementScore = () => {
     this.setState({ score: this.state.score + 1 });
   };
 
+  incrementQuestionIndex = () => {
+    this.setState({
+      currentQuestionIndex: this.state.currentQuestionIndex + 1,
+    });
+  };
+
   handleSubmit = () => {
     const scoreDetails = {
       score: this.state.score,
-      //un comment when the db includes topic_id
-      topic_id: this.props.topic
-      // username;
+      topic_id: this.props.topic,
     };
 
     console.log(scoreDetails);
-  
-     API.saveScores(scoreDetails).then((res) => { 
+
+    API.saveScores(scoreDetails).then((res) => {
       //api call to post score to databas and in the .then we have the below
       console.log(res);
       this.props.history.push("/scoreboard");
@@ -66,38 +64,41 @@ class Questions extends Component {
   };
 
   render() {
-    return (
-      <div className="mt-5 mb-5">
-          <div>
-            {this.state.question[this.state.currentQuestionIndex].question}
+    const { questions, currentQuestionIndex } = this.state;
+    if (questions[currentQuestionIndex]) {
+      return (
+        <div className="mt-5 mb-5">
+          <div className="mb-5">
+            <h2>{questions[currentQuestionIndex].question}</h2>
+            <ListGroup>
+              {questions[currentQuestionIndex].Answers.map((answer) => {
+                return (
+                  <ListGroup.Item
+                    key={answer.id}
+                    data-correct={answer.correct}
+                    onClick={this.handleSelection}
+                  >
+                    {answer.answer}
+                  </ListGroup.Item>
+                );
+              })}
+            </ListGroup>
           </div>
-        {/* {this.state.questions.map((question, index) => {
-          return (
-            <div key={index} className="mb-5">
-              <h2>{question.question}</h2>
-              <ListGroup>
-                {question.Answers.map((answer) => {
-                  return (
-                    <ListGroup.Item
-                      key={answer.id}
-                      data-correct={answer.correct}
-                      onClick={this.handleSelection}
-                    >
-                      {answer.answer}
-                    </ListGroup.Item>
-                  );
-                })}
-              </ListGroup>
-            </div>
-          );
-        })} */}
-         <p className="score-tag">Your Score is :{this.state.score}</p>
-          <Button variant="outline-dark" onClick={this.handleSubmit}>Submit</Button>
-       
-      </div>
-    );
+        </div>
+      );
+    } else if (questions.length > 0 && currentQuestionIndex > questions.length - 1) {
+      return (
+        <div className="mt-5 mb-5">
+          <p>{this.state.score > 7 ? "Great job!" : "Nice try!"}</p>
+          <p className="score-tag">Your Score is: {this.state.score}</p>
+
+          <Button variant="outline-dark" onClick={this.handleSubmit}>
+            Continue
+          </Button>
+        </div>
+      );
+    } else return null;
   }
 }
-
 
 export default withRouter(Questions);
