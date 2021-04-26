40ac5d158f181e8a40ffc6ea6732ae1be71c3953
abissier <71886314+abissier@users.noreply.github.com>
2021-04-25 22:33:57 -0700

    added aws and back button to scoreboard

--- client/src/Components/ScoreBoard/ScoreBoard.js
+++ client/src/Components/ScoreBoard/ScoreBoard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import MainBtn from '../MainBtn/MainBtn';
 import Logout from '../Logout/Logout';
 import './style.css';
 import API from '../../utils/API';
@@ -7,20 +8,22 @@ function ScoreBoard(props) {
 	const [ jsResults, setJsResults ] = useState([]);
 	const [ nodejsResults, setNodejsResults ] = useState([]);
 	const [ reactResults, setReactResults ] = useState([]);
+	const [ awsResults, setAwsResults ] = useState([]);
 
 	useEffect(() => {
-		Promise.all([ API.getScores(1), API.getScores(2), API.getScores(3) ]).then((results) => {
+		Promise.all([ API.getScores(1), API.getScores(2), API.getScores(3), API.getScores(4) ]).then((results) => {
 			setJsResults(results[0].data);
 			setNodejsResults(results[1].data);
 			setReactResults(results[2].data);
+			setAwsResults(results[3].data);
 		});
 	}, []);
 
 	return (
 		<div className="scoreboard-page">
+			<MainBtn />
 			<Logout {...props} />
 			<h1 className="scores-header">Top Scores</h1>
-			{/* Confitional Render based on state length amp through array1-3*/}
 			<div className="row">
 				<div className="column">
 					<h1 className="scoreboard-header">React</h1>
@@ -45,6 +48,16 @@ function ScoreBoard(props) {
 				<div className="column">
 					<h1 className="scoreboard-header">Node.js</h1>
 					{nodejsResults.map((result) => (
+						<li className="scores-li" key={result.id}>
+							<div className="score-info">
+								{result.username} {result.score}
+							</div>
+						</li>
+					))}
+				</div>
+				<div className="column">
+					<h1 className="scoreboard-header">AWS</h1>
+					{awsResults.map((result) => (
 						<li className="scores-li" key={result.id}>
 							<div className="score-info">
 								{result.username} {result.score}
